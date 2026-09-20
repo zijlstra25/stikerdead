ff95b2570e37da92d01d5c1bcd0e90c2468f4586
zijlstra25 <gamboag992@gmail.com>
2026-09-20 17:50:59 -0300

    Restaurar escala del icono sin recorte

--- app/src/main/java/com/stikerdead/keyboard/StickerKeyboardService.java
+++ app/src/main/java/com/stikerdead/keyboard/StickerKeyboardService.java
@@ -178,8 +178,8 @@ public class StickerKeyboardService extends InputMethodService {
         ImageButton stickers = new ImageButton(this);
         stickers.setContentDescription("Stickers");
         stickers.setImageResource(R.drawable.sticker_icon);
-        stickers.setScaleType(ImageButton.ScaleType.CENTER_CROP);
-        stickers.setPadding(0, 0, 0, 0);
+        stickers.setScaleType(ImageButton.ScaleType.CENTER_INSIDE);
+        stickers.setPadding(2, 2, 2, 2);
         stickers.setBackgroundColor(Color.WHITE);
         stickers.setOnClickListener(v -> switchToStickerMode());
         toolbar.addView(stickers, toolbarSquareParams());
